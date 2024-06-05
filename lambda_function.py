898e3c4a428c6a2118d95500cf5ce4f4c290d585
g702900 <arthur.bargas@sagemcom.com>
2024-06-05 14:31:23 +0200

    ca marche

--- lambda_function.py
+++ lambda_function.py
@@ -1,4 +1,63 @@
+import cv2
 import json
+import base64
+import numpy as np
+import mediapipe as mp
+import tensorflow as tf
+from mediapipe.tasks import python
+from mediapipe.tasks.python import vision
+
+# Constants
+DESIRED_HEIGHT = 480
+DESIRED_WIDTH = 480
+BG_COLOR = (192, 192, 192)  # gray
+MASK_COLOR = (255, 255, 255)  # white
+
+
+# Function to segmentate image
+def segmentate(file):
+    base_options = python.BaseOptions(model_asset_path="hair_segmenter.tflite")
+    options = vision.ImageSegmenterOptions(
+        base_options=base_options, output_category_mask=True
+    )
+
+    with vision.ImageSegmenter.create_from_options(options) as segmenter:
+        decoded_image = base64.b64decode(file)
+        img_np = np.frombuffer(decoded_image, np.uint8)
+        img = cv2.imdecode(img_np, cv2.IMREAD_COLOR)
+
+        image = mp.Image(image_format=mp.ImageFormat.SRGB, data=img)
+        segmentation_result = segmenter.segment(image)
+
+        category_mask = segmentation_result.category_mask
+
+        image_data = image.numpy_view()
+        fg_image = np.zeros(image_data.shape, dtype=np.uint8)
+        fg_image[:] = MASK_COLOR
+        bg_image = np.zeros(image_data.shape, dtype=np.uint8)
+        bg_image[:] = BG_COLOR
+
+        condition = np.stack((category_mask.numpy_view(),) * 3, axis=-1) > 0.2
+        output_image = np.where(condition, fg_image, bg_image)
+
+        prediction = predict(img)[0][0]
+        limits = [0.002, 0.1, 0.4, 0.95, 0.97, 0.991, 1]
+        scale = np.where(prediction < limits)[0][0] + 1
+
+        return scale, output_image
+
+
+def predict(image):
+    # Redimensionner et normaliser l'image
+    data = cv2.resize(image, (64, 64))
+    data = cv2.cvtColor(data, cv2.COLOR_BGR2RGB)
+    ims = np.reshape(data, (1, 64, 64, 3)) / 255.0
+
+    print(ims.shape)
+    model = tf.keras.models.load_model("models/bald_classifity.h5")
+    res = model.predict(ims)
+
+    return res
 
 
 def lambda_handler(event, context):
@@ -6,12 +65,11 @@ def lambda_handler(event, context):
     body = json.loads(event["body"])
     # Vérifier si le fichier est présent dans l'événement
     if "file" not in body.keys():
-
         return {
             "statusCode": 400,
             "headers": {"Content-Type": "application/json"},
-            "body": json.dumps(event),
-            # "body": json.dumps({"error": f"No file provided {entry}"}),
+            # "body": json.dumps(event),
+            "body": json.dumps({"error": f"No file provided"}),
         }
 
     file = body["file"]
@@ -22,7 +80,7 @@ def lambda_handler(event, context):
         return {
             "statusCode": 200,
             "headers": {"Content-Type": "application/json"},
-            "body": json.dumps(event),
+            "body": json.dumps({"nordwood_scale": str(scale)}),
         }
     except Exception as e:
         # Gérer les exceptions et log pour débogage
@@ -32,21 +90,3 @@ def lambda_handler(event, context):
             "headers": {"Content-Type": "application/json"},
             "body": json.dumps({"error": "Internal server error", "details": str(e)}),
         }
-
-    # Réponse par défaut (ceci ne devrait pas être atteint)
-    return {
-        "statusCode": 200,
-        "body": json.dumps({"blob": "blobn"}),
-    }
-
-
-# Exemple de la fonction segmentate (désactivée pour l'instant)
-def segmentate(file):
-    # Fonction segmentate désactivée pour ce débogage
-    return 1, None
-
-
-# Exemple de la fonction predict (désactivée pour l'instant)
-def predict(image):
-    # Fonction predict désactivée pour ce débogage
-    return [1]
--- req.py
+++ req.py
@@ -13,98 +13,24 @@ image_path = "test_data/3.png"
 # Lire l'image en binaire
 # with open(image_path, "rb") as image_file:
 #     print("image chargée", image_file)
-#     file = {"file": image_file}
+#     payload = {"file": image_file}
 #     # headers = {"Authorization": "Bearer " + api_key}
-#     response = requests.post(url, files=file)  # , headers=headers)
+#     # response = requests.post(url, files=file)  # , headers=headers)
 
 with open(image_path, "rb") as image_file:
     image_data = image_file.read()
     encoded_image = base64.b64encode(image_data).decode("utf-8")
     # print(encoded_image)
-encoded_image = "f"
-payload = {"file": encoded_image}
+    # encoded_image = "f"
+    payload = {"file": encoded_image}
 
 # Envoyer la requête POST avec les données de l'image
 response = requests.post(url, json=payload)
 
-bl = '{"file":"f"}'
-print(json.loads(bl)["file"])
-
 # Vérifiez la réponse de l'API
 if response.status_code == 200:
     data = response.json()
-    print(data)
-    # nordwood_scale = data["nordwood_scale"]
-    # print(f"Nordwood Scale: {nordwood_scale}")
+    nordwood_scale = data["nordwood_scale"]
+    print(f"Nordwood Scale: {nordwood_scale}")
 else:
     print(f"Failed to get a response, status code: {response.json()}")
-    # print(json.loads(response.json()["error"]["body"]))
-    with open("blaf.txt", "w", encoding="utf-8") as file:
-        file.write(response.text)
-    # print(type(response.json()["error"]))
-
-
-fhg = {
-    "resource": "/oma",
-    "path": "/oma",
-    "httpMethod": "POST",
-    "headers": {
-        "Accept": "*/*",
-        "Accept-Encoding": "gzip, deflate",
-        "Content-Type": "application/json",
-        "Host": "tv1wdm122i.execute-api.eu-west-3.amazonaws.com",
-        "User-Agent": "python-requests/2.32.3",
-        "X-Amzn-Trace-Id": "Root=1-666052a7-5c68285a2904f61d491011d0",
-        "X-Forwarded-For": "79.174.206.120",
-        "X-Forwarded-Port": "443",
-        "X-Forwarded-Proto": "https",
-    },
-    "multiValueHeaders": {
-        "Accept": ["*/*"],
-        "Accept-Encoding": ["gzip, deflate"],
-        "Content-Type": ["application/json"],
-        "Host": ["tv1wdm122i.execute-api.eu-west-3.amazonaws.com"],
-        "User-Agent": ["python-requests/2.32.3"],
-        "X-Amzn-Trace-Id": ["Root=1-666052a7-5c68285a2904f61d491011d0"],
-        "X-Forwarded-For": ["79.174.206.120"],
-        "X-Forwarded-Port": ["443"],
-        "X-Forwarded-Proto": ["https"],
-    },
-    "queryStringParameters": None,
-    "multiValueQueryStringParameters": None,
-    "pathParameters": None,
-    "stageVariables": None,
-    "requestContext": {
-        "resourceId": "r8di3v",
-        "resourcePath": "/oma",
-        "httpMethod": "POST",
-        "extendedRequestId": "Y5HaQFJXCGYEQvA=",
-        "requestTime": "05/Jun/2024:11:57:27 +0000",
-        "path": "/default/oma",
-        "accountId": "471112953630",
-        "protocol": "HTTP/1.1",
-        "stage": "default",
-        "domainPrefix": "tv1wdm122i",
-        "requestTimeEpoch": 1717588647730,
-        "requestId": "c1557cb0-17e6-4360-86f5-33fa76028a77",
-        "identity": {
-            "cognitoIdentityPoolId": None,
-            "accountId": None,
-            "cognitoIdentityId": None,
-            "caller": None,
-            "sourceIp": "79.174.206.120",
-            "principalOrgId": None,
-            "accessKey": None,
-            "cognitoAuthenticationType": None,
-            "cognitoAuthenticationProvider": None,
-            "userArn": None,
-            "userAgent": "python-requests/2.32.3",
-            "user": None,
-        },
-        "domainName": "tv1wdm122i.execute-api.eu-west-3.amazonaws.com",
-        "deploymentId": "829hm3",
-        "apiId": "tv1wdm122i",
-    },
-    "body": '{"file": "f"}',
-    "isBase64Encoded": False,
-}
